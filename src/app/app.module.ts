4c64de44a0a686bdefde609afa6416c346fa2bca
Richard LEGROS <rlegros.cdi@FORMATION.LOCAL>
2019-02-06 16:05:51 +0400

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { MyaccountComponent } from './myaccount/myaccount.component';
+import { AnnonceComponent } from './annonce/annonce.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    MyaccountComponent,
+    AnnonceComponent
   ],
   imports: [
     BrowserModule,
