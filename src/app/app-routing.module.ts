23876d8f0fffccc06be0897ca0995332e4390e65
Loïc BATONNET <lbatonnet.cdi@FORMATION.LOCAL>
2019-02-07 10:37:06 +0400

    Routing + add bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component'
+import { AnnonceComponent } from './components/annonce/annonce.component'
 
 const routes: Routes = [
-
+  { path: '', redirectTo: '/homepage', pathMatch: 'full' },
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'annonce', component: AnnonceComponent },
+//{ path: 'heroes', component: HeroesComponent },
+ // { path: 'heroes', component: HeroesComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { MyaccountComponent } from './myaccount/myaccount.component';
-import { AnnonceComponent } from './annonce/annonce.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { MyaccountComponent } from './components/myaccount/myaccount.component';
+import { AnnonceComponent } from './components/annonce/annonce.component';
+import {MatIconModule} from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +22,10 @@ import { AnnonceComponent } from './annonce/annonce.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatMenuModule,MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
