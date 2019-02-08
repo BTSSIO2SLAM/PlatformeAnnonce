2ba045c55e852f7dc5f8eb010f968d771a7b9d67
Loïc BATONNET <lbatonnet.cdi@FORMATION.LOCAL>
2019-02-08 09:36:18 +0400

    loib

--- src/app/service/annonce.service.ts
+++ src/app/service/annonce.service.ts
@@ -1,32 +1,44 @@
 import { Injectable } from '@angular/core';
 import { Annonce } from '../class/annonce';
-
+import { Photos } from '../class/photos';
 @Injectable({
   providedIn: 'root'
 })
 export class AnnonceService {
 
   listeDesAnnonces: Array<Annonce>;
+  listePhoto: Array<Photos>;
 
   constructor() {
 
-    this.listeDesAnnonces = [
-      {"id": 1, "titre": "Switch","details":'Fatigué de rager ! Je vend ma switch :)',"prix": 200, "vendeur": 'Richard', "categorie": 'Console', "ville": 'Saint denis'},
+    this.listePhoto = [
+      { "id": 1, "libelle": 'switch.jpg', "chemin": '/assets', "annonce": 1 },
+      { "id": 2, "libelle": 'moto.JPG', "chemin": '/assets', "annonce": 2 }
 
     ];
 
-   }
+    this.listeDesAnnonces = [
+      { "id": 1, "titre": "Switch", "details": 'Fatigué de rager ! Je vend ma switch :)', "prix": 200, "vendeur": 1, "categorie": 'Console', "ville": 'Saint denis' },
+      { "id": 2, "titre": "Moto", "details": 'Fatigué de rager ! Je vend ma switch :)', "prix": 200, "vendeur": 1, "categorie": 'Console', "ville": 'Saint denis' },
 
-   getAnnonce(): Array<Annonce>{
+    ];
 
+  }
+
+  getAnnonce(): Array<Annonce> {
     return this.listeDesAnnonces;
-   }
+  }
 
-   createAnnonce(annonce : Annonce){
+  getPhotoAnnonce(): Array<Photos> {
 
-      this.listeDesAnnonces.push(annonce);
-      //return true;
-   }
+    return this.listePhoto;
+  }
+
+  createAnnonce(annonce: Annonce) {
+
+    this.listeDesAnnonces.push(annonce);
+    //return true;
+  }
 
 
 }
